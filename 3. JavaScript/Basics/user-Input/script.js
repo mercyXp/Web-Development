51066d68fd401426e4e0e7ff760b6e8824d8b278
MercyXp <munzenzimercy2024@gmail.com>
2025-04-29 11:27:10 +0700

    user input

--- 3. JavaScript/Basics/user-Input/script.js
+++ 3. JavaScript/Basics/user-Input/script.js
@@ -3,6 +3,11 @@
 //1. THE EASY WAY: window prompt
 //2. THE PROFESSIONAL WAY: HTML textbox
 
+let username = window.prompt("What is your username?");
+console.log(username);
+
+
+
 let firstName, lastName;
 
 document.getElementById("mySubmit").onclick = function(){
