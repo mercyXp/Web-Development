f8724c8016d48d9bd8abbfa6d8cdb65b0c6bdd83
MercyXp <munzenzimercy2024@gmail.com>
2025-05-01 05:04:44 +0700

    class

---
+++ 3. JavaScript/OOP/Class/class.js
@@ -0,0 +1,12 @@
+class Products{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+    displayProduct(){
+        console.log(`Product: ${this.name}`);
+        console.log(`Product: ${this.price}`);
+    }
+}
+const product1 = new Products("Shirt", 19.99);
+product1.displayProduct();
